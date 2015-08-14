d820842c5d6b0bf52b80871838fa9f17b154ee32
Josh Perez <josh@goatslacker.com>
2015-08-14 03:18:33 -0400

    Fixing up css

--- src/components/ChannelList.jsx
+++ src/components/ChannelList.jsx
@@ -1,4 +1,3 @@
-import classname from 'classnames'
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import connectToStores from 'alt/utils/connectToStores'
@@ -11,15 +10,10 @@ class ChannelList extends Component {
   renderChannel = (channel) => {
     const { name } = channel
     const channelName = name.substr(1)
-    const classString = classname({
-      'channel-name': true,
-      'channel-name-active': this.props.channel == channelName
-    })
-    console.log(this.props.channel, name)
 
     return (
-      <li key={name} className={classString}>
-        <Link to={`/${channelName}`}>{name}</Link>
+      <li key={name}>
+        <Link className="channel-name" to={`/${channelName}`}>{name}</Link>
       </li>
     )
   }
